4a649dc2f188278626b2d0046eaa9915e6c6d601
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2023-06-16 09:57:31 +0500

    cors config change

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const app = express()
 
 connectToDB()
 
-app.use(cors())
+app.use(cors({ origin: true, credentials: true }))
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
