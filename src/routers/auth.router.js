8b7260ae7771d0ff156dc3ab5e2fcf01945cf309
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2023-06-17 16:07:24 +0500

    get user info

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -3,6 +3,7 @@ const AuthController = require('../modules/Auth/auth.controller.js')
 
 const Controller = new AuthController()
 
+router.get('/me', Controller.getMe)
 router.post('/auth/login', Controller.login)
 router.post('/auth/reset_password', Controller.resetPassword)
 router.post('/auth/change_password/', Controller.changePassword)
