9fb56e54acfe2badd572426404df730058036146
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2023-07-05 11:30:04 +0500

    df

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -6,7 +6,7 @@ const Controller = new AuthController()
 router.get('/me', Controller.getMe)
 router.post('/auth/login', Controller.login)
 router.post('/auth/reset_password', Controller.resetPassword)
-router.post('/auth/change_password/', Controller.changePassword)
+router.post('/auth/change_password', Controller.changePassword)
 router.post('/auth/logout', Controller.logout)
 
 module.exports = router
